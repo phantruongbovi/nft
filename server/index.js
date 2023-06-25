846a94a004576030c0e067d77d3c8ed71e5b21d8
Phan Thanh Trường <truong.phan@mservice.com.vn>
2023-06-25 17:43:09 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/auth");
+
+const authRouter = require("./routes/auth");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.czrhoe2.mongodb.net/nft?retryWrites=true&w=majority`
+    );
+
+    console.log("DB connected");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
